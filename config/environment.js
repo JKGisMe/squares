683dea625a93248c574f3c31a14017ee85da58fd
jkgisme <incarnadin@gmail.com>
2015-09-22 12:20:00 -0500

    getting started

--- config/environment.js
+++ config/environment.js
@@ -1,7 +1,7 @@
 /* jshint node: true */
 
 module.exports = function(environment) {
-  var ENV = {
+  const ENV = {
     modulePrefix: 'squares',
     environment: environment,
     baseURL: '/',
@@ -12,6 +12,20 @@ module.exports = function(environment) {
         // e.g. 'with-controller': true
       }
     },
+    metricsAdapters: [
+      {
+        name: 'GoogleAnalytics',
+        config: {
+          id: 'UA-67543151-1'
+        }
+      },
+      {
+        name: 'Mixpanel',
+        config: {
+          token: 'afe41ad1fa5676d34782dbcd135d4168'
+        }
+      }
+    ],
 
     APP: {
       // Here you can pass flags/options to your application instance
@@ -25,6 +39,17 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV.contentSecurityPolicy = {
+      'default-src': "'none'",
+      'script-src': "'self' http://www.google-analytics.com http://cdn.mxpnl.com",
+      'font-src': "'self' https://fonts.gstatic.com",
+      'connect-src': "'self' http://localhost:4000 http://api.mixpanel.com",
+      'img-src': "'self' http://www.google-analytics.com https://stats.g.doubleclick.net data:",
+      'style-src': "'self' 'unsafe-inline'",
+      'media-src': "'self'",
+      'object-src': "'self'",
+      'frame-src': "'self'"
+    };
   }
 
   if (environment === 'test') {
